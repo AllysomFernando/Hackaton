f7c14001ca7e1124c76932d05729562fd63fd17a
MateusFindev <finprogramador@gmail.com>
2024-02-08 18:43:41 -0300

    addding dependecies

--- Hackaton/App.js
+++ Hackaton/App.js
@@ -1,20 +1,64 @@
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import AuthProvider, {AuthContext} from './src/contexts/auth';
+import { Poppins_500Medium, Poppins_700Bold } from '@expo-google-fonts/poppins';
+import { useFonts, Roboto_500Medium, Roboto_700Bold } from '@expo-google-fonts/roboto';
+import * as SplashScreen from 'expo-splash-screen';
+import CardScreen from './src/screens/Card';
+import HomeScreen from './src/screens/Home';
+import Login from './src/screens/Login';
+import MySplashScreen from './src/screens/SplashScreen';
+import EventsScreen from './src/screens/Events';
+import CertificateScreen from './src/screens/Certificate';
+import StartScreen from './src/screens/StartScreen';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import 'react-native-reanimated';
+import 'react-native-gesture-handler';
+import Toast from 'react-native-toast-message';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+
+
+function App() {
+  const Stack = createNativeStackNavigator();
+  let [fontsLoaded] = useFonts({
+    Poppins_500Medium, Poppins_700Bold, Roboto_500Medium, Roboto_700Bold
+  })
+  React.useEffect(() => {
+    async function prepare() {
+      await SplashScreen.preventAutoHideAsync();
+    }
+    prepare();
+  }, []);
+  const onLayoutRootView = React.useCallback(async () => {
+    if (fontsLoaded) {
+      await SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
+  return (<>
+
+    <StatusBar style="light" />
+    <NavigationContainer onReady={onLayoutRootView}>
+          <Stack.Navigator>
+            <Stack.Screen options={{ headerShown: false, gestureEnabled: false }} name="Splash" component={MySplashScreen} />
+            <Stack.Screen options={{ headerShown: false, gestureEnabled: false }} name="Start" component={StartScreen} />
+            <Stack.Screen options={{ headerShown: false, gestureEnabled: false }} name="LoginScreen" component={Login} />
+            <Stack.Screen options={{ headerShown: false, gestureEnabled: true }} name="Home" component={HomeScreen} />
+            <Stack.Screen options={{ headerShown: false, }} name="Card" component={CardScreen} />
+            <Stack.Screen options={{ headerShown: false, }} name="Events" component={EventsScreen} />
+            <Stack.Screen options={{ headerShown: false, }} name="Certificate" component={CertificateScreen} />
+          </Stack.Navigator>
+    </NavigationContainer>
+    <Toast />
+  </>
+
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
+export default App;
---
+++ Hackaton/src/components/Title/Titile.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Title = ({ text }) => (
+    <p>{text}</p>
+);
+
+export default Title;
