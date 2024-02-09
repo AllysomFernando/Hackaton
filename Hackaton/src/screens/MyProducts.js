967efeabebfb9f009d6a6e7f59e074507683d069
unknown <allysom.renczenczen18@gmail.com>
2024-02-08 22:21:25 -0300

    MyProducts Update

---
+++ Hackaton/src/screens/MyProducts.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { View, Text , StyleSheet} from 'react-native'
+import Header from '../components/Header/Header';
+import MyTitle from '../components/Title/Title';
+
+function MyProducts() {
+    return (
+    <View>
+        <Header/>
+        <MyTitle title="Seus Produtos"/>
+        
+    </View>)
+};
+
+
+
+export default MyProducts;
